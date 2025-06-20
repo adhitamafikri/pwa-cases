4d5fdd25ba975a6d95426f83708ee57c7fb43fc2
adhitamafikri <adhitamafikri.dev@gmail.com>
2025-06-20 11:47:00 +0700

    update

---
+++ 01-vite-pwa/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite-plugin-pwa/react" />
+
+declare module 'virtual:pwa-register' {
+  import type { RegisterSWOptions } from 'vite-plugin-pwa/types'
+
+  export type { RegisterSWOptions }
+
+  export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>
+}
--- 01-vite-pwa/vite.config.ts
+++ 01-vite-pwa/vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "prompt",
-      devOptions: {
-        enabled: true,
-      },
-      strategies: "generateSW",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      VitePWA({
+        strategies: "generateSW",
+        registerType: "autoUpdate",
+        injectRegister: "auto",
+        devOptions: {
+          enabled: process.env.NODE_ENV === "development",
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        },
+      }),
+    ],
+    server: {
+      host: env.VITE_APP_HOST || "localhost",
+      port: env.VITE_APP_PORT ? Number(env.VITE_APP_PORT) : 3000,
+    },
+  };
 });
